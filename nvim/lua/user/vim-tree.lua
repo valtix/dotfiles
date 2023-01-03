f5d6777607a080d9c43888e4dbcc60d753b886a7
valtix <ivan.valtierrez@gmail.com>
2023-01-03 16:22:28 -0600

    latest commit

--- nvim/lua/user/vim-tree.lua
+++ nvim/lua/user/vim-tree.lua
@@ -27,7 +27,7 @@ nvim_tree.setup({
 	view = {
 		adaptive_size = false,
 		centralize_selection = false,
-		width = 120,
+		width = 70,
 		hide_root_folder = false,
 		side = "left",
 		preserve_window_proportions = false,
