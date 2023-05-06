a6b2bd2626812a246966a22e05297d26bbe93219
valtix <ivan.valtierrez@gmail.com>
2023-05-05 21:37:05 -0500

    added new changes

--- nvim/lua/user/plugins/coc-vim.lua
+++ nvim/lua/user/plugins/coc-vim.lua
@@ -170,8 +170,6 @@ vim.opt.statusline:prepend("%{coc#status()}%{get(b:,'coc_current_function','')}"
 local opts = {silent = true, nowait = true}
 -- Show all diagnostics
 keyset("n", "<space>a", ":<C-u>CocList diagnostics<cr>", opts)
--- Manage extensions
-keyset("n", "<space>e", ":<C-u>CocList extensions<cr>", opts)
 -- Show commands
 keyset("n", "<space>c", ":<C-u>CocList commands<cr>", opts)
 -- Find symbol of current document
